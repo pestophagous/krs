b4b407287103dda85d97f55d076f3255837327ad
pestophagous <pestophagous@users.noreply.github.com>
2025-10-04 11:08:26 -0700

    finally approaching 'parity' with original 'step zero' of python skeleton

--- krs_gensheet/src/krs_base/set.rs
+++ krs_gensheet/src/krs_base/set.rs
@@ -1,1 +1,38 @@
-pub struct Set {}
+#[derive(Clone)]
+pub struct Item {
+    pub unique_id: String,
+    pub answer: String,
+    pub tags: Vec<String>,
+    pub prompt: String,
+}
+
+pub struct Set {
+    ordered_items: Vec<Item>,
+}
+
+impl Set {
+    pub fn new() -> Set {
+        let mut the_set = Set {
+            ordered_items: Vec::new(),
+        };
+
+        let fake_item = Item {
+            unique_id: "a844695".to_owned(),
+            answer: "x=1, x=-1".to_owned(),
+            tags: [
+                "gcf".to_owned(),
+                "factoring".to_owned(),
+                "solve-for-x".to_owned(),
+            ]
+            .into(),
+            prompt: "$$ 4x^2 - 4 = 0 $$".to_owned(),
+        };
+        the_set.ordered_items.push(fake_item);
+
+        the_set
+    }
+
+    pub fn get_all_items(&self) -> Vec<Item> {
+        self.ordered_items.clone()
+    }
+}
--- krs_gensheet/src/main.rs
+++ krs_gensheet/src/main.rs
@@ -1,7 +1,9 @@
-use crate::krs_inputloader::ExtractText;
+//use crate::krs_base::set::Set; // oh!
+use crate::krs_inputloader::ExtractText; // #[warn(unused_imports)] can i do as error?
 
 use clap::Parser;
 
+mod krs_base;
 mod krs_inputloader;
 
 /// Simple program to greet a person
@@ -18,10 +20,25 @@ struct Args {
 }
 
 fn main() {
+    // FUTURE: we want config file and/or CLI args (as the krs-b has), so check out:
+    //    https://stackoverflow.com/questions/55133351/is-there-a-way-to-get-clap-to-use-default-values-from-a-file
+    //    https://www.reddit.com/r/rust/comments/1ecnriw/command_line_env_vars_and_configs/
+    //    https://github.com/clap-rs/clap/discussions/2763#discussioncomment-7633613
     let args = Args::parse();
+
+    let input_set = krs_base::set::Set::new();
+    let mut input_items = input_set.get_all_items();
 
     for _ in 0..args.count {
         println!("Hello {}!", args.name);
+    }
+
+    for item in input_items.iter() {
+        println!("");
+        println!("unique_id {}", item.unique_id);
+        println!("answer {}", item.answer);
+        println!("tags {:#?}", item.tags);
+        println!("prompt {}", item.prompt);
     }
 }
 
